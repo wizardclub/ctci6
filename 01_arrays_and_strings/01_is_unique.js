894f15eea9efe38fc8156217093acf033fc4ea55
Denzil Kriekenbeek <denzil@cpxi.com>
2016-09-07 17:41:08 -0400

    refactored questions into files

---
+++ 01_arrays_and_strings/01_is_unique.js
@@ -0,0 +1,16 @@
+/*
+1.1 Is Unique:
+Implement an algorithm to determine if a string has all unique characters. 
+
+What if you cannot use additional data structures?
+*/
+
+
+function check_unique(str){
+  return false;
+}
+
+
+function check_unique_in_place(str){
+  return false;
+}
