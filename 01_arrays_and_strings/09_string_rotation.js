e97f8706d28c92b2608f234c51b414d195133b75
U-CPXINTERACTIVE\arogers <arogers@arogers-nyc-w.cpxinteractive.com>
2016-09-20 18:59:51 -0400

    adding my solution for question 09

--- 01_arrays_and_strings/09_string_rotation.js
+++ 01_arrays_and_strings/09_string_rotation.js
@@ -6,9 +6,68 @@ Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 usin
 one call to isSubstring (e.g., "waterbottle" is a rotation of "erbottlewat").
 */
 
+function isSubstring(input, staticInput) {
+    "use strict";
+    
+    if (input === staticInput) {
+        console.log("Exact Match");
+        return true;
+    }
+  
+    if (input.length != staticInput.length) {
+        console.log("Input length does not match staticInput length");
+        return false;
+    }
+  
+    var inputSort = input.split('');
+    inputSort = inputSort.sort();
+    inputSort = inputSort.toString();
+        console.log("inputSort is " + inputSort);
+    var staticInputSort = staticInput.split('');
+    staticInputSort = staticInputSort.sort();
+    staticInputSort = staticInputSort.toString();
+        console.log("staticInputSort is " + staticInputSort);
+  
+    if (inputSort != staticInputSort) {
+        console.log("Input does not have the same elements as staticInput");
+        return false;
+    }
+  
+    inputArr = input.split("");
+    staticInputArr = staticInput.split("");
+    var checkpoint = [];
+  
+    var j = 0;
+    while (j < input.length) {
+        var index = 0;
+        index = input.indexOf(staticInput[0],j);
+        checkpoint.push(index);
+        j++;
+    }
+  
+    checkpoint = checkpoint.filter(function(elem, index, self) {
+        return index === self.indexOf(elem);
+    });
+  
+        console.log("checkpoints are " + checkpoint);
+  
+    for (var i = 0; i < checkpoint.length; i++) {
+            console.log("Starting slice check");
+        var currentCheck = checkpoint[i];
+        var beginning = inputArr.slice(currentCheck);
+        beginning = beginning.join('');
+        var end = inputArr.slice(0, currentCheck);
+        end = end.join('');
+            console.log("beginning is " + beginning);
+            console.log("end is " + end);
+        if (beginning + end === staticInput) {
+            console.log(beginning + end + " == " + staticInput);
+        return true;
+        }
+    }
+    return false;
+  
+}  
 
-
-function isSubstring(s1, s2){
-
-  return false;
-}
+wordRotationCheck("ordw","word");
+This looks like a JavaScript file. Click this bar to format it.No 4
